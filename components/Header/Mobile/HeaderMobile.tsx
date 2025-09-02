e2e7641c519d856d366b9150639f47b653eb7cd1
SotnikovSS <sotnikovss90@gmail.com>
2025-09-02 13:10:22 +0200

    go

--- components/Header/Mobile/HeaderMobile.tsx
+++ components/Header/Mobile/HeaderMobile.tsx
@@ -4,6 +4,10 @@ import Link from "next/link";
 import css from "./HeaderMobile.module.css";
 import { IoMdMenu } from "react-icons/io";
 import { AiOutlineCloseSquare } from "react-icons/ai";
+import { LuDock } from "react-icons/lu";
+import { LuHousePlug } from "react-icons/lu";
+import { LuFireExtinguisher } from "react-icons/lu";
+import Image from "next/image";
 
 export default function HeaderMobile() {
   const [open, setOpen] = useState(false);
@@ -17,21 +21,18 @@ export default function HeaderMobile() {
 
       <button
         className={css.burger}
-        onClick={() => setOpen((v) => !v)}
+        onClick={() => setOpen((prev) => !prev)}
         aria-expanded={open}
         aria-controls="mobile-nav"
-      > {!open ? (<IoMdMenu size={26}/>) : (<AiOutlineCloseSquare size={26}/>)}
-        <span />
-        <span />
-        <span />
+      > {!open ? (<IoMdMenu size={26} className={css.burgerIcon}/>) : (<AiOutlineCloseSquare size={26} className={css.burgerIcon}/>)}
       </button>
 
       {open && (
-        <nav id="mobile-nav" className={css.menu}>
-          <ul>
-            <li><Link href="/fipron" onClick={() => setOpen(false)}>Fipron</Link></li>
-            <li><Link href="/certificates" onClick={() => setOpen(false)}>Сертифікати</Link></li>
-            <li><Link href="/#contacts" onClick={() => setOpen(false)}>Контакти</Link></li>
+        <nav id="mobile-nav" className={`${css.menu} ${open ? css.menuOpen : ""}`}>
+          <ul className={css.navList}>
+            <li className={css.navItem}><Link className={css.navLink} href="/fipron" onClick={() => setOpen(false)}><LuFireExtinguisher className={css.navIcon} size={24}/> Firesi</Link></li>
+            <li className={css.navItem}><Link className={css.navLink} href="/certificates" onClick={() => setOpen(false)}><LuHousePlug className={css.navIcon} size={24}/> Fipron</Link></li>
+            <li className={css.navItem}><Link className={css.navLink} href="/#contacts" onClick={() => setOpen(false)}><LuDock className={css.navIcon} size={24}/> Сертифікати</Link></li>
           </ul>
         </nav>
       )}
