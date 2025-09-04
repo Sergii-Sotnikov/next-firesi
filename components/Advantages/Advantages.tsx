45815a0d5598e58e47d52d10f257a29dc22fcebf
SotnikovSS <sotnikovss90@gmail.com>
2025-09-04 16:59:30 +0200

    go

--- components/Advantages/Advantages.tsx
+++ components/Advantages/Advantages.tsx
@@ -5,19 +5,18 @@ export default function Advantages() {
   return (
     <section className={css.advantages}>
       <div className={css.containerAdvantages}>
-        <h2 className={css.advantagesTitle}>
-          Європейські інновації для вашої безпеки
-        </h2>
-        <span className={css.advantagesTitleText}>
-          переваги
+        <h2 className={css.advantagesTitle}>Європейські&nbsp;інновації</h2>
+        <div className={css.wrapperMobile}>
+          <p className={css.advantagesTitleText}>переваги</p>
           <Image
             className={css.pictureHero}
-            src="/img/firesi@2x.png"
+            src="/img/firesi_mobile@2x.png"
             width={346}
             height={145}
             alt="Вогнегасник FIPRON"
-          /> 
-        </span>
+          />
+          </div>
+        <p className={css.advantagesTitle}> для&nbsp;вашої&nbsp;безпеки</p>
       </div>
     </section>
   );
