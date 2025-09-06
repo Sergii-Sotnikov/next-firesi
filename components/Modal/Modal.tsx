802fc578f7db277543a4abcf9281fa5a30ba41cd
SotnikovSS <sotnikovss90@gmail.com>
2025-09-06 21:33:52 +0200

    go

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { createPortal } from "react-dom";
 import css from "./Modal.module.css";
 import { MdClose } from "react-icons/md";
@@ -9,77 +11,54 @@ interface ModalProps {
 }
 
 export default function Modal({ closeModal, children }: ModalProps) {
-
-  const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === e.currentTarget) closeModal();
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
   };
 
   useEffect(() => {
-
-    const onKeyDown = (e: KeyboardEvent) => {
-      if (e.key === "Escape") closeModal();
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        closeModal();
+      }
     };
-    document.addEventListener("keydown", onKeyDown);
 
-
-    const scrollY = window.scrollY;
-    const scrollbarW = window.innerWidth - document.documentElement.clientWidth;
-
-    const html = document.documentElement;
-    const body = document.body;
-
-    html.style.overflow = "hidden";
-    html.style.paddingRight = `${scrollbarW}px`;
-
-    body.style.position = "fixed";
-    body.style.top = `-${scrollY}px`;
-    body.style.left = "0";
-    body.style.right = "0";
-    body.style.width = "100%";
-    body.style.paddingRight = `${scrollbarW}px`;
-    body.style.overscrollBehavior = "contain";
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
 
     return () => {
-      document.removeEventListener("keydown", onKeyDown);
-
-      html.style.overflow = "";
-      html.style.paddingRight = "";
-
-      body.style.position = "";
-      body.style.top = "";
-      body.style.left = "";
-      body.style.right = "";
-      body.style.width = "";
-      body.style.paddingRight = "";
-      body.style.overscrollBehavior = "";
-
-
-      window.scrollTo(0, scrollY);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "";
     };
   }, [closeModal]);
 
+  useEffect(() => {
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
 
-  const root = document.getElementById("modal-root") as HTMLDivElement;
+    document.body.style.overflow = "hidden";
+    document.body.style.paddingRight = `${scrollbarWidth}px`;
+
+    return () => {
+      document.body.style.overflow = "";
+      document.body.style.paddingRight = "";
+    };
+  }, []);
 
   return createPortal(
-    <div
-      className={css.backdrop}
-      onClick={handleBackdropClick}
-      role="dialog"
-      aria-modal="true"
-    >
-      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+    <div className={css.backdrop} onClick={handleBackdropClick}>
+      <div className={css.modal}>
         <button
           className={css.closeButton}
           onClick={closeModal}
           aria-label="Close modal"
-          type="button"
         >
           <MdClose size={24} color="#ffffff" />
         </button>
         {children}
       </div>
     </div>,
-    root
+    document.getElementById("modal-root") as HTMLDivElement
   );
 }
