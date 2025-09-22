5fcbdab03ceb2f120b80af12e427346764a95c69
SotnikovSS <sotnikovss90@gmail.com>
2025-09-22 17:32:07 +0200

    go

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,34 @@
 import { NextResponse } from "next/server";
-export function middleware(req: Request) {
-  const url = new URL(req.url);
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl; // удобнее, чем new URL(req.url)
+  const host = req.headers.get("host") || url.host;
+  const proto =
+    req.headers.get("x-forwarded-proto")?.toLowerCase() ||
+    url.protocol.replace(":", "").toLowerCase();
+
+
+  if (host === "www.firetech.com.ua") {
+    url.host = "firetech.com.ua";
+    return NextResponse.redirect(url, { status: 301 });
+  }
+
+
+  if (proto !== "https") {
+    url.protocol = "https:";
+    return NextResponse.redirect(url, { status: 301 });
+  }
+
+
   if (url.pathname === "/&") {
     url.pathname = "/";
     return NextResponse.redirect(url, { status: 301 });
   }
+
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/:path*"],
+};
