0cab238ac43690777afaa81b6eaa3d22f76a8915
SotnikovSS <sotnikovss90@gmail.com>
2025-09-25 21:31:09 +0200

    go

--- components/VideoMainPage/VideoMainPage.tsx
+++ components/VideoMainPage/VideoMainPage.tsx
@@ -6,10 +6,11 @@ export default function VideoMainPage(){
       <div className={css.container}>
         <div className={css.videoWrapper}>
 <iframe
-  src="https://www.youtube.com/embed/wYesTZs_8-o?rel=0&modestbranding=1&playsinline=1&controls=1"
+  src="https://www.youtube-nocookie.com/embed/wYesTZs_8-o?rel=0&modestbranding=1&playsinline=1&controls=1"
   title="YouTube video player"
   loading="lazy"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  sandbox="allow-scripts allow-same-origin allow-presentation allow-popups"
   referrerPolicy="strict-origin-when-cross-origin"
   allowFullScreen
 />
